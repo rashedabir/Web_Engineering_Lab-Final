1610b456a2b9348fbbb05b6ee59c2d8330cfd3db
rashedabir <rashed15-2155@diu.edu.bd>
2021-08-22 13:55:15 +0600

    login system

---
+++ login/index.php
@@ -0,0 +1,62 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet" />
+    <title>Home</title>
+</head>
+
+<style>
+.navbar {
+    background-image: linear-gradient(120deg, #2980b9, #8e44ad);
+    background-attachment: fixed;
+}
+</style>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container">
+            <a class="navbar-brand text-white" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active text-white" aria-current="page" href="./index.php">Home</a>
+                </div>
+                <div class="navbar-nav ms-auto">
+                    <a class="btn btn-danger" aria-current="page" href="logout.php" tite="Logout">Log out</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container">
+        <?php
+if($_SESSION["name"]) {
+?>
+        <h2 class="my-5">Welcome <?php echo $_SESSION["name"]; ?>...</h2>
+        <?php
+}else echo "<h1>Please login first .</h1>";
+?>
+    </div>
+
+    <!-- script -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
---
+++ login/login.php
@@ -0,0 +1,63 @@
+<?php
+    session_start();
+    $message="";
+    if(count($_POST)>0) {
+        $con = mysqli_connect('127.0.0.1:3306','root','','labfinal') or die('Unable To connect');
+        $result = mysqli_query($con,"SELECT * FROM users WHERE email='" . $_POST["email"] . "' and password = '". $_POST["password"]."'");
+        $row  = mysqli_fetch_array($result);
+        if(is_array($row)) {
+        $_SESSION["id"] = $row['id'];
+        $_SESSION["name"] = $row['name'];
+        } else {
+         $message = "Invalid Username or Password!";
+        }
+    }
+    if(isset($_SESSION["id"])) {
+    header("Location:index.php");
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet" />
+    <title>Login</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="login mx-auto my-5 py-5">
+            <form name="frmUser" method="post" action="" class="bg-white p-3">
+                <h1 class="text-center py-5 my-3">Login</h1>
+                <div class="message text-center mb-3"><?php if($message!="") { echo $message; } ?></div>
+                <div class="form-floating mb-3">
+                    <input type="text" class="form-control" name="email" id="floatingInput"
+                        placeholder="name@example.com" />
+                    <label for="floatingInput">Email address</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <input type="password" class="form-control" name="password" id="floatingPassword"
+                        placeholder="Password" />
+                    <label for="floatingPassword">Password</label>
+                </div>
+                <button type="submit" class="btn btn-primary w-100" value="submit" name="submit">sign in</button>
+                <h5 class="text-center py-5 my-3">Don't have an account? Sign Up</h5>
+            </form>
+        </div>
+    </div>
+    <!-- script -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
